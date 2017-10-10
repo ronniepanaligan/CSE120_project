cc9148b48749c2a10cd55aa518ddaf20c3d7e1a3
Ronnie <rpanaligan@ucmerced.edu>
2017-10-09 17:14:42 -0700

    updates
    lul

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+//this is the code to start the server which is ran by express.js
 var express = require('express');
+var session = require('express-session');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
 var app = express();
+var port = process.env.PORT || 3000;
+
+var db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cse120', { useMongoClient: true });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+//middleware stuff
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('home');
 });
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(port, function(){
+    console.log('running on Port '+ port);
 });
