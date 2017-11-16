d9d0162973f9403cf0316ebefea3ad6483c476fb
Ronnie <rpanaligan@ucmerced.edu>
2017-11-15 19:30:00 -0800

    Webpack updates

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,20 +1,52 @@
-var config = require('./webpack.config.js');
 var webpack = require('webpack');
+var path = require('path');
+// React v.16 uses some newer JS functionality, so to ensure everything
+// works across all browsers, we're adding babel-polyfill here.
+const TransferWebpackPlugin = require('transfer-webpack-plugin');
+require('babel-polyfill');
 
-config.plugins.push(
-  new webpack.DefinePlugin({
-    "process.env": {
-      "NODE_ENV": JSON.stringify("production")
-    }
-  })
-);
-
-config.plugins.push(
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    }
-  })
-);
-
-module.exports = config;
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    loaders: [
+      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve('./'),
+      path.resolve('./node_modules'),
+    ],
+    extensions: ['.js','.scss'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        // suppresses warnings, usually from module minification
+        warnings: false,
+      },
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // Transfer Files
+  ]
+};
